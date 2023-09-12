81530bd13429737df1dcdc19dba956cab95b1673
Zayn Stark <102648673+simp-0x@users.noreply.github.com>
2023-09-12 18:40:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,27 +22,27 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.botname = "Roughers Bot" 
+global.author = "Simp"  
+global.packname = "★彡[ᴅᴀʀᴋ ʀᴏᴜɢʜᴇʀꜱ ʙᴏᴛ]彡★"  
+global.myweb = "https://t.me/dark_roughers"
+global.footer = "©\t" + "★彡[ᴅᴀʀᴋ ʀᴏᴜɢʜᴇʀꜱ]彡★"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
-global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
-global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+global.socialText = `*⪧⪢ Please join this Channel for by bot's announcement.*`
+global.socialLink = `*⪧⪢ https://t.me/dark_roughers 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "Simp"    
+global.ownernumber = ["923095708264"]
+global.ytname = "YT: Techy Vault"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.region = "Pakistan"
+global.state = "Punjab" // your state name
+global.timezone = "Asia/Islamabad" // search on google if you don't know the timezone of your country //
+global.instagramId = "https://instagram.com/jibran.ahmed.3734?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"
+global.email = "neerubajwa478@gmail.com"
 
 
 // Greeting messages
